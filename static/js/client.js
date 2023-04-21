ea7449c07f67fd494dc1a0aa0928155401a752ae
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-04-21 22:36:52 +0530

    Update client.js

--- static/js/client.js
+++ static/js/client.js
@@ -61,7 +61,7 @@ const getname = () => {
         let name = userNameInput.value;
         setUsernameButton.innerHTML = `Please Wait<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">  <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/></svg>`;
         localStorage.setItem('username', name);
-        setInterval(() => {
+        setTimeout(() => {
             popup.style.display = 'none';
             socket.emit('new-user-joined', name);
         }, 2000);
