083e4152174a583e385bf490a307ec00eae2ecbb
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-04-23 14:13:58 +0530

    Update client.js

--- static/js/client.js
+++ static/js/client.js
@@ -74,8 +74,10 @@ setUsernameButton.addEventListener('click', () => {
 
 // If a new user joins, receive his/her name from the server
 socket.on('user-joined', name => {
-    append(`${name} joined the chat`, 'left')
-})
+    if(name != 'rahul'){
+        append(`${name} joined the chat`, 'left');
+    };
+});
 
 // If server sends a message, receive it
 socket.on('receive', data => {
@@ -89,7 +91,9 @@ socket.on('receive', data => {
 
 // If a user leaves the chat, append the info to the container
 socket.on('left', name => {
-    append(`${name} left the chat`, 'left')
+    if(name != 'rahul' || name != 'null'){
+        append(`${name} left the chat`, 'left');
+    };
 })
 
 // If the form gets submitted, send server the message
